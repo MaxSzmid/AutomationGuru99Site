483dd2cf36ce148fed1a9a11cccb5c89f45001bd
Max <maxszmid@gmail.com>
2020-08-12 13:57:15 -0300

    Adding some commets on the code and the test-case document with 3 cases done

--- src/main/java/automateWebSiteGuru/RunTest.java
+++ src/main/java/automateWebSiteGuru/RunTest.java
@@ -7,41 +7,40 @@ import browser.SelectBrowsers;
 public class RunTest {
 
 	public static void main(String[] args) throws InterruptedException {
-		// select the browser
+		// Select the browser
 		WebDriver driver = SelectBrowsers.selectBrowser("chrome");
 		driver.get("http://demo.guru99.com/V1/index.php");
+		//You can check all the ducumentation in the folder Documentation ---> Gherkins and testCases"
 
-		// creating an object to execute the "Login" test
-
-		// testCase 1
+		// Test Case (Guru99_TC_1)
 		LoginPage page = new LoginPage(driver);
 		page.setUser("mngr276741");
 		page.setPassword("YsaqAjY");
 		page.clickOnButtonLogin();
 		System.out.println("test case 1 excecuted");
 		Thread.sleep(4000);
-		// creating an object to execute the "Home page" test
-		// testCase 2
+
+		// Test Case (Guru99_TC_2)
 		HomePage homePage = new HomePage(driver);
 		homePage.clickOnNewCustomer();
 		System.out.println("test case 2 excecuted");
 		Thread.sleep(4000);
-		// creating an object to execute the "Create new customer" test
-		// testCase 3
+
+		// Test Case(Guru99_TC_3)
 		NewCustomerPage newCustomer = new NewCustomerPage(driver);
-		newCustomer.setName("Carlos");
+		newCustomer.setName("Carlos Szmid");
 		newCustomer.setGender("famale");
 		newCustomer.setDOB("02/08/1992");
 		newCustomer.setAdress("Independencia 59");
-		newCustomer.setCity("Cordoba");
-		newCustomer.setState("Argentina");
-		newCustomer.setPIN("515251");
-		newCustomer.setPhone("3564665543");
+		newCustomer.setCity("Saldán");
+		newCustomer.setState("Cordoba");
+		newCustomer.setPIN("313131");
+		newCustomer.setPhone("1122334455");
 		newCustomer.setEmail("testEmail@email.com");
 		System.out.println("test case 3 excecuted");
 		Thread.sleep(4000);
-		// creating an object to execute the "Create new account test"
-		// testCase 4
+
+		// Test Case(Guru99_TC_4)
 		NewAccount newAccount = new NewAccount(driver);
 		newAccount.goToNewAccount(driver);
 		// As the button doesn't work well, i decided to redirect manually to the "New account" page, but this isn't OK
